95f3ed745b9d570634873d4b95c3a82319fe01fb
Navod <54998118+navodveduth@users.noreply.github.com>
2024-09-23 20:09:19 +0530

    Add cancellation request controller and customer requests controller

---
+++ Backend/Backend/Models/CancellationRequest.cs
@@ -0,0 +1,40 @@
+/*
+* This class represents a cancellation request made by a customer.
+*/
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Backend.Models;
+
+public class CancellationRequest
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("OrderId")]
+    public required string OrderId { get; set; }
+
+    [BsonElement("CustomerId")]
+    public required string CustomerId { get; set; }
+
+    [BsonElement("RequestDate")]
+    public DateTime RequestDate { get; set; }
+
+    [BsonElement("Status")] // Pending, Approved, Denied
+    public required string Status { get; set; } = "Pending";
+
+    [BsonElement("Reason")]
+    public required string Reason { get; set; }
+
+    [BsonElement("ProcessedBy")]
+    public required string ProcessedBy { get; set; } // CSR or Admin ID
+
+    [BsonElement("ProcessedDatse")]
+    public DateTime? ProcessedDate { get; set; }
+
+    [BsonElement("DecisionNote")]
+    public string? DecisionNote { get; set; } // Reason for approval/denial
+}
