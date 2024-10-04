31096c1a9e78d7ac5bce73a0cd989edd3a0e9e8f
Sandaru-IT21001352 <sandarukarunarathna118@gmail.com>
2024-10-05 04:27:06 +0530

    role based side bar navigation item filtering

--- Frontend/src/components/common/Sidebar/Sidebar.jsx
+++ Frontend/src/components/common/Sidebar/Sidebar.jsx
@@ -14,11 +14,15 @@ import {
 } from "react-icons/bs";
 import { Link, useLocation } from "react-router-dom";
 import "./Sidebar.css";
+import { useAuth } from "../../../provider/authProvider";
 
 const Sidebar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
   const [openItems, setOpenItems] = useState({});
+  const {
+    user: { role },
+  } = useAuth();
 
   const toggleOpen = (index) => {
     setOpenItems((prev) => ({
@@ -28,33 +32,83 @@ const Sidebar = () => {
   };
 
   const navLinks = [
-    { title: "Dashboard", path: "/dashboard", icon: <BsGrid /> },
+    {
+      title: "Dashboard",
+      path: "/dashboard",
+      icon: <BsGrid />,
+      roles: ["admin", "vendor", "csr"],
+    },
     {
       title: "Catalog",
       icon: <BsCommand />,
+      roles: ["admin", "vendor"],
       subLinks: [
-        { title: "Products", path: "/products", icon: <BsGrid3X3Gap /> },
-        { title: "Categories", path: "/categories", icon: <BsGrid3X2Gap /> },
+        {
+          title: "Products",
+          path: "/products",
+          icon: <BsGrid3X3Gap />,
+          roles: ["admin", "vendor"],
+        },
+        {
+          title: "Categories",
+          path: "/categories",
+          icon: <BsGrid3X2Gap />,
+          roles: ["admin"],
+        },
       ],
     },
-    { title: "Orders", path: "/orders", icon: <BsBag /> },
+    {
+      title: "Orders",
+      path: "/orders",
+      icon: <BsBag />,
+      roles: ["admin", "vendor"],
+    },
     {
       title: "Users",
       icon: <BsPeople />,
+      roles: ["admin", "csr"],
       subLinks: [
         {
           title: "Approval Requests",
           path: "/customers/approval-requests",
           icon: <BsPerson />,
+          roles: ["admin", "csr"],
         },
-        { title: "Customers", path: "/customers", icon: <BsPerson /> },
-        { title: "Vendors", path: "/vendors", icon: <BsPerson /> },
-        { title: "Staff", path: "/staff", icon: <BsPerson /> },
+        {
+          title: "Customers",
+          path: "/customers",
+          icon: <BsPerson />,
+          roles: ["admin", "csr"],
+        },
+        {
+          title: "Vendors",
+          path: "/vendors",
+          icon: <BsPerson />,
+          roles: ["admin"],
+        },
+        {
+          title: "Staff",
+          path: "/staff",
+          icon: <BsPerson />,
+          roles: ["admin"],
+        },
       ],
     },
-    { title: "Inventory", path: "/inventory", icon: <BsCardList /> },
-    { title: "Reports", path: "/reports", icon: <BsClipboardCheck /> },
+    {
+      title: "Inventory",
+      path: "/inventory",
+      icon: <BsCardList />,
+      roles: ["admin", "vendor"],
+    },
+    {
+      title: "Reports",
+      path: "/reports",
+      icon: <BsClipboardCheck />,
+      roles: ["admin"],
+    },
   ];
+
+  const filteredNavLinks = navLinks.filter((link) => link.roles.includes(role));
 
   return (
     <div className="sidebar">
@@ -62,7 +116,7 @@ const Sidebar = () => {
         <img src="/logo.png" alt="logo" />
       </div>
       <div className="sidebarContent">
-        {navLinks.map((link, index) => (
+        {filteredNavLinks.map((link, index) => (
           <div key={index}>
             {link.subLinks ? (
               <div className="navItem" onClick={() => toggleOpen(index)}>
@@ -86,18 +140,20 @@ const Sidebar = () => {
 
             {link.subLinks && openItems[index] && (
               <div className="subLinks">
-                {link.subLinks.map((subLink, subIndex) => (
-                  <Link
-                    to={subLink.path}
-                    key={subIndex}
-                    className={`navItem subItem ${
-                      currentPath === subLink.path ? "active" : ""
-                    }`}
-                  >
-                    {subLink.icon}
-                    <span className="ms-2">{subLink.title}</span>
-                  </Link>
-                ))}
+                {link.subLinks
+                  .filter((subLink) => subLink.roles.includes(role))
+                  .map((subLink, subIndex) => (
+                    <Link
+                      to={subLink.path}
+                      key={subIndex}
+                      className={`navItem subItem ${
+                        currentPath === subLink.path ? "active" : ""
+                      }`}
+                    >
+                      {subLink.icon}
+                      <span className="ms-2">{subLink.title}</span>
+                    </Link>
+                  ))}
               </div>
             )}
           </div>
