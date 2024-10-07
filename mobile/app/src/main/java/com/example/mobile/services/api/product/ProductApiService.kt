6a918fa6bed7430002448f42cf9e3778333df80a
IT20159030 <it20159030@my.sliit.lk>
2024-10-07 22:17:55 +0530

    Refactor ProductApiService to include methods for retrieving products by category and retrieving product categories

--- mobile/app/src/main/java/com/example/mobile/services/api/product/ProductApiService.kt
+++ mobile/app/src/main/java/com/example/mobile/services/api/product/ProductApiService.kt
@@ -1,9 +1,11 @@
 package com.example.mobile.services.api.product
 
 import com.example.mobile.dto.Product
+import com.example.mobile.dto.ProductCategory
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Path
 
 /*
 * A service interface for products APIs
@@ -19,6 +21,10 @@ interface ProductApiService {
     @GET("Product/vendor/{vendorId}/active")
     suspend fun getProductsByVendor(@Query("vendorId") vendorId: String): Response<List<Product>>
 
+    @GET("ProductCategory")
+    suspend fun getProductCategories(): Response<List<ProductCategory>>
 
+    @GET("Product/category/{categoryId}/active")
+    suspend fun getProductsByCategory(@Path("categoryId") categoryId: String): Response<List<Product>>
 
 }
